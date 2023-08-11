228e2f901a0c03cb49fac590d45793c34df21aea
김용준 <rladydwns129@naver.com>
2023-08-11 19:45:54 +0900

    add .gitignore

--- kimsuhyuk/Main.java
+++ kimsuhyuk/Main.java
@@ -1,3 +1,5 @@
+package kimsuhyuk;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
--- parkhayoon/Main.java
+++ parkhayoon/Main.java
@@ -1,3 +1,5 @@
+package parkhayoon;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
